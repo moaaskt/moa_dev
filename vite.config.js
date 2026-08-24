e9e7926b20b5f71c5c6dcf132f8c161cbe43d62a
moaaskt <moacirmayer@hotmail.com>
2026-08-24 20:05:01 -0300

    perf(fcp): add instant inline fallback skeleton, enable cssCodeSplit, and ensure max minification

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    cssCodeSplit: true,
+    minify: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
